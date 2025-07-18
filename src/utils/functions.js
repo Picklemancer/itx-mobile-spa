ea84be6f49f14a5620971de5c24edb016d14992f
Picklemancer <152328166+Picklemancer@users.noreply.github.com>
2025-07-18 11:10:03 +0200

    add functions utils file

---
+++ src/utils/functions.js
@@ -0,0 +1,13 @@
+export const debounce = (callback, wait) => {
+    let timerId;
+
+    return (...args) => {
+
+        clearTimeout(timerId);
+
+        timerId = setTimeout(() => {
+            callback(...args);
+        }, wait);
+
+    };
+};
